6763416790a972869bc30b5a6d6971665cd1b775
github_username <email_address>
2022-04-08 17:49:45 +0530

    fgf

---
+++ bashscript/test22.sh
@@ -0,0 +1,90 @@
+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
+https://github.com/siddhgopal/shell-bash-script-file.git
+
+#!/bin/bash
+clear
+#syntax
+read -p "enter your name :"gopal
+
+if [ "$name" = "gopal"];then
+echo "welcome"
+
+else
+        echo "who are you"
+
+fi
+
+#####################
+
+#!/bin/bash
+clear
+
+#syntax
+
+#-eq   = equal to
+#-ne   = not equal to
+#-gt   = greate then
+#-le   = less then or equal to
+
+read -p "enter a number greater than 10: " num
+
+if ["$sum" -gt 10 ]; then  or ["$sum" -ge 10 ]
+        echo "you are currect"
+
+else
+        echo "you are incorrect"
+
+fi
+
+output=incurrect
+output=currrect
+
+##################################
+#!/bin/bash
+clear
+#this is a comment
+
+#  code for facebook
+
+#this line echos / prints helloworld
+
+# no need to use data type
+
+x=90
+
+echo "variable value is :$x"
+
+    x="nitin"
+
+echo "the new value is : $x"
+
+####################
+#!/bin/bash
+
+clear
+
+read -p "enter your name :"name
+
+echo "hii $name ! how are you today ?"
+
+##########
+addition
+
+#!/bin/bash
+
+clear
+
+echo -e "\t\tLearning Arithmetic  operators"
+
+#read -p "Enter Tow Numbers"num1 num2
+
+x=5
+y=6
+
+add=$(($num1 + $num2))
+
+echo "Addition is : $add"
+#######################
+
+
+
